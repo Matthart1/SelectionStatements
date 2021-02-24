01fb8c76b9659afc75b9a7d71f77e9831479eec9
Matt Hart <mp.hart047@gamil.com>
2021-02-23 20:27:26 -0600

    (Selection statements)

---
+++ SelectionStatements/SelectionStatements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var r = new Random();
+            Console.WriteLine("Guess the number!!");
+            var userInput = int.Parse(Console.ReadLine());
+            int favNumber = r.Next(1, 69);
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("You got to go up!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Go down a bit!");
+            }
+            else if (userInput == favNumber)
+            {
+                Console.WriteLine("Great job!!!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind.");
+            }
+
+
+        }
+    }
+}
+ 
