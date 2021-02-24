42ba7b6aa3bf16b0abf868df0bfc1a832b0fec1e
Matt Hart <mp.hart047@gamil.com>
2021-02-23 20:56:39 -0600

    (Selection Statements 2.0)

--- SelectionStatements/SelectionStatements/Program.cs
+++ SelectionStatements/SelectionStatements/Program.cs
@@ -28,6 +28,32 @@ namespace SelectionStatements
                 Console.WriteLine("Nevermind.");
             }
 
+            Console.WriteLine("What was your favorite subject in school?");
+            string subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "math":
+                    Console.WriteLine("Math is pretty cool.");
+                    Console.WriteLine("What a hoot!");
+                    break;
+                case "science":
+                    Console.WriteLine("Now thats a subject that'll take you places!");
+                    break;
+                case "literature":
+                    Console.WriteLine("All great minds enjoy literature!");
+                    break;
+                case "english":
+                    Console.WriteLine("Lame, Loser subject");
+                    break;
+                case "history":
+                    Console.WriteLine("Must remember the history!!");
+                    break;
+                default:
+                    Console.WriteLine("I have to opinion.");
+                    break;
+            }
+
 
         }
     }
